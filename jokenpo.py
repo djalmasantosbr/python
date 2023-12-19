348b39d0df353fffbb78c49b6771e4e252d2c529
djalmasantosbr <djalmasantosbr@gmail.com>
2023-12-19 18:06:13 -0300

    Consegui solucinar o problema do placar colocando a formula dentro da função, chamando o return com uma tupla

--- jokenpo.py
+++ jokenpo.py
@@ -11,16 +11,20 @@ lance = ['Papel','Pedra', 'Tesoura']
 def jokepo(voce,computador):
     if (voce == 'Papel' and computador == 'Papel') or (voce == 'Pedra' and computador == 'Pedra') or (voce == 'Tesoura' and computador == 'Tesoura')  :
         resultado = "Empatou..."
+        placar = 0
     elif voce == 'Papel' and computador == 'Pedra':
-        resultado = "Você ganhou"    
+        resultado = "Você ganhou"   
+        placar = 1 
     elif voce == 'Pedra' and computador == 'Tesoura':
         resultado = "Você ganhou"  
+        placar = 1
     elif voce == 'Tesoura' and computador == 'Papel':
-        resultado = "Você ganhou"     
+        resultado = "Você ganhou"    
+        placar = 1 
     else:
         resultado = "Computador ganhou"  
-    return resultado
-
+        placar = 2
+    return (resultado, placar)
 
     
 while True:
@@ -30,8 +34,8 @@ while True:
     print("============================================")
     print("")
     print("PLACAR:")
-    print("Você: {}".format(placar_humano))
-    print("Computador: {}".format(placar_maquina))
+    print("Você: {}".format(int(placar_humano)))
+    print("Computador: {}".format(int(placar_maquina)))
     print("")
     print("")
 
@@ -45,18 +49,19 @@ while True:
     print("============================")
     print("Sua jogada: {}".format(voce))
     print("Jogada do computador: {}".format(computador))
-    print(jokepo(voce,computador))
+    print(jokepo(voce,computador)[0])
     print("============================")
 
-    if   jokepo(voce,computador) == "Você ganhou!":
+    if jokepo(voce,computador)[1] == 1:
         placar_humano = placar_humano + 1
-    elif jokepo(voce,computador) == "Computador ganhou!": 
+    elif jokepo(voce,computador)[1] == 2: 
         placar_maquina = placar_maquina + 1
-        
+    
+    print("")    
     print("Deseja jogar novamente? 0 - SIM | 1 NÃO")
     jogo = int(input())
 
-    if jogo == 1:
+    if jogo != 0:
         os.system("cls")
         break
     else:

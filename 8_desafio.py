c96bdb35f7c2768e79ca6e4a1fa3e96bc3d59ae3
djalmasantosbr <djalmasantosbr@gmail.com>
2024-01-09 20:35:32 -0300

    Mostra a pasta e seu tamanho em GB

---
+++ 8_desafio.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import os
+
+def retorna_tamanho_arquivos(caminho, profundidade=1, tamanho_linha=0):
+    for diretorio in caminho.glob('*'):
+        if diretorio.is_dir() and not diretorio.name.startswith('.'):
+            tamanho_diretorio = 0
+            for arquivo in diretorio.glob('**/*'):
+                if arquivo.is_file():
+                    tamanho_diretorio += os.path.getsize(arquivo)
+            print('--' * tamanho_linha, diretorio.name, round(tamanho_diretorio / 1024 / 1024, 2), 'mb' )
+            if profundidade > 1:
+                retorna_tamanho_arquivos(diretorio, profundidade-1, tamanho_linha+1)
+            
+caminho = Path.home() / 'OneDrive' / 'Documentos'
+retorna_tamanho_arquivos(caminho, profundidade=2)
